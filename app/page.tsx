da1b40538d581b77a5d809ba316a54eff38b48cb
Daniel Krsak <krsak.daniel@gmail.com>
2023-07-19 16:41:03 +0200

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,200 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import axios from "axios";
+import { Montserrat } from "next/font/google";
+import { useEffect, useState } from "react";
+
+const API_KEY = "4828c422f2-b257572eb7-ry1pb0";
+const BASE_URL = "https://api.fastforex.io/fetch-one";
+const CURRENCIES = ["EUR", "USD", "CZK"];
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const MainPage = () => {
+  type Callback = (...args: any[]) => void;
+  type Currency = "EUR" | "USD" | "CZE";
+
+  const [amount, setAmount] = useState("");
+  const [convertedAmount, setConvertedAmount] = useState("");
+  const [convertFromCurrency, setConvertFromCurrency] =
+    useState<Currency>("EUR");
+  const [convertToCurrency, setConvertToCurrency] = useState<Currency>("USD");
+
+  const [conversionData, setConversionData] = useState<
+    {
+      before: string;
+      after: string;
+      fromCurrency: string;
+      toCurrency: string;
+    }[]
+  >([]);
+
+  const handleFromCurrencyChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setConvertFromCurrency(e.target.value as Currency);
+  };
+
+  const handleToCurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setConvertToCurrency(e.target.value as Currency);
+  };
+
+  const handleConversion = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const conversion = {
+      before: amount,
+      after: convertedAmount,
+      fromCurrency: convertFromCurrency,
+      toCurrency: convertToCurrency,
+    };
+    setConversionData((prevData) => [...prevData, conversion]);
+
+    setAmount("");
+  };
+
+  const debounce = <T extends Callback>(callback: T, delay: number) => {
+    let timerId: NodeJS.Timeout;
+
+    return (...args: Parameters<T>) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+  };
+
+  useEffect(() => {
+    const fetchCurrentRates = async () => {
+      const res = await axios.get(
+        `${BASE_URL}?from=${convertFromCurrency.toUpperCase()}&to=${convertToCurrency.toUpperCase()}&api_key=${API_KEY}`
+      );
+
+      setConvertedAmount(
+        (Number(amount) * res.data.result[convertToCurrency]).toFixed(2)
+      );
+    };
+
+    const debouncedFetchCurrentRates = debounce(fetchCurrentRates, 500);
+
+    if (amount) {
+      debouncedFetchCurrentRates();
+    }
+  }, [amount, convertFromCurrency, convertToCurrency]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="h-full w-full flex justify-center bg-gray-700">
+      <div className="w-full max-w-4xl">
+        <h1 className={`text-7xl text-center mt-24 ${montserrat.className}`}>
+          Currency Converter
+        </h1>
+        <div className="flex justify-between w-full mt-32">
+          <form
+            className="flex flex-col items-center gap-4 w-full"
+            onSubmit={handleConversion}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text text-xl text-white">Amount</span>
+              </label>
+              <input
+                min={0}
+                onChange={(e) => setAmount(e.target.value)}
+                value={amount}
+                type="number"
+                placeholder="Amount to be converted.."
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text text-xl text-white">
+                  Convert from
+                </span>
+              </label>
+              <select
+                className="select select-bordered w-full max-w-xs"
+                value={convertFromCurrency}
+                onChange={handleFromCurrencyChange}
+              >
+                {CURRENCIES.map((currency) => (
+                  <option key={currency}>{currency}</option>
+                ))}
+              </select>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text text-xl text-white">
+                  Convert to
+                </span>
+              </label>
+              <select
+                className="select select-bordered w-full max-w-xs"
+                value={convertToCurrency}
+                onChange={handleToCurrencyChange}
+              >
+                {CURRENCIES.filter(
+                  (currency) => currency !== convertFromCurrency
+                ).map((currency) => (
+                  <option key={currency}>{currency}</option>
+                ))}
+              </select>
+            </div>
+            {amount && (
+              <div className="flex gap-8">
+                <button className="btn btn-accent" type="submit">
+                  Convert
+                </button>
+                <div className="flex flex-col">
+                  <div className="flex gap-8 italic opacity-30">
+                    <p>Before</p>
+                    <p>
+                      {amount} {convertFromCurrency}
+                    </p>
+                  </div>
+                  <div className="flex gap-8 italic opacity-30">
+                    <p>After</p>
+                    <p>
+                      {convertedAmount} {convertToCurrency}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </form>
+          <div className="w-full">
+            <h1 className="label-text text-xl text-white">
+              Table of Conversions
+            </h1>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Before</th>
+                    <th>After</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {conversionData.map((data, idx) => (
+                    <tr key={idx}>
+                      <th>{idx + 1}</th>
+                      <td>
+                        {data.before} {data.fromCurrency}
+                      </td>
+                      <td>
+                        {data.after} {data.toCurrency}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default MainPage;
